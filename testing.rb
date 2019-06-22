9b8342a9719126292ae5f92892c28e907ed81c02
addisonpohl <addison.c.pohl@gmail.com>
2019-06-22 01:19:33 -0500

    Testing commits through Atom

---
+++ testing.rb
@@ -0,0 +1,9 @@
+#git instructions in Ruby // Just for fun
+
+puts "-Create a repository in GitHub"
+puts "-Intiate Git in terminal using 'git init'"
+puts "-Collect remote add origin link and paste it in terminal"
+puts "-Add files to be included in the repository"
+puts "-Commit changes"
+puts "-Verify changes are added by using status"
+puts "-Push Commits to repository"
