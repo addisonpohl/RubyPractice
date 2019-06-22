d52f566e951925ce5fd7cff5bc53522e21d0df70
addisonpohl <addison.c.pohl@gmail.com>
2019-06-22 13:26:13 -0500

    second commit

--- testing.rb
+++ testing.rb
@@ -1,9 +1,9 @@
 #git instructions in Ruby // Just for fun
 
-puts "-Create a repository in GitHub"
-puts "-Intiate Git in terminal using 'git init'"
-puts "-Collect remote add origin link and paste it in terminal"
-puts "-Add files to be included in the repository"
-puts "-Commit changes"
-puts "-Verify changes are added by using status"
-puts "-Push Commits to repository"
+puts "-Create a repository in GitHub
+ -Intiate Git in terminal using 'git init'
+ -Collect remote add origin link and paste it in terminal
+ -Add files to be included in the repository
+ -Commit changes
+ -Verify changes are added by using status
+ -Push Commits to repository"
