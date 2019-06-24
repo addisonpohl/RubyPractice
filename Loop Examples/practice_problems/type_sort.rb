46aea75145ee2ae0a2435db7da1f1a973a48e1da
addisonpohl <addison.c.pohl@gmail.com>
2019-06-23 20:36:55 -0500

    Adding a practice problem which sorts by type

---
+++ Loop Examples/practice_problems/type_sort.rb
@@ -0,0 +1,27 @@
+types = [2, "two", 2.0]
+
+def sort(an_array)
+#Sort objects from an array by type and place them in their own array
+  num = 0
+  str = ""
+  flt = 0.0
+  a_num = Array.new
+  a_str = Array.new
+  a_flt = Array.new
+
+  an_array.each do |value|
+    if value.class == num.class
+      num += value
+      a_num.push(num)
+    elsif value.class == str.class
+      str += value
+      a_str.push(str)
+    elsif value.class == flt.class
+      flt += value
+      a_flt.push(flt)
+    end
+  end
+  print(a_num, a_str, a_flt)
+end
+
+sort(types)
