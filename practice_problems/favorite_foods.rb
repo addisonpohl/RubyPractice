0ed2e86b51bd4bf24bd22897b87323b3ddfa351a
addisonpohl <addison.c.pohl@gmail.com>
2019-06-27 01:03:39 -0500

    Adding practice problem

---
+++ practice_problems/favorite_foods.rb
@@ -0,0 +1,21 @@
+
+def food()
+  puts "What are your 5 favorite foods?"
+
+  user_foods = []
+  5.times do |food|
+    food = gets.chomp
+    user_foods.push(food)
+  end
+  p user_foods
+
+  user_foods.each { |food| puts "I love #{food}s!"}
+
+  count = 0
+  user_foods.each do |food|
+  count += 1
+  puts count.to_s + ". " + food
+  end
+end
+
+food()
