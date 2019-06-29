b363b1346a5e94753cc5412414e248752da71b97
addisonpohl <addison.c.pohl@gmail.com>
2019-06-28 22:07:26 -0500

    Block Method examples

---
+++ block_examples/basic_block.rb
@@ -0,0 +1,9 @@
+def block_method
+  puts "This is the first line in block_method."
+  yield
+  puts "This statement is after the yield keyword."
+end
+
+block_method do
+  puts "This statement is called from the block."
+end
