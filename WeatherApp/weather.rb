d966f90d8488b3c939525c46d0bea7dad1eec8de
addisonpohl <addison.c.pohl@gmail.com>
2019-07-14 23:09:06 -0500

    Add weather api: http request practice

---
+++ WeatherApp/weather.rb
@@ -0,0 +1,25 @@
+require "http"
+
+#pull weather data from a specefied location
+#verify weather is displaying correctly
+#display data in the console
+
+def get_weather(city)
+  #collects the weather conditions from the specefied location
+  response = HTTP.get("http://api.openweathermap.org/data/2.5/weather?q=#{city},us&units=imperial&APPID=INSERT")
+  weather_data = response.parse
+  return weather_data
+end
+
+
+#collect user's cityG
+puts "What city are you in?"
+city = gets.chomp
+
+#Call method with given location
+get_weather(city)
+
+#print weather conditions and tempature
+conditions = get_weather(city)["weather"][0]["description"]
+temp = get_weather(city)["main"]["temp"]
+puts "The weather today is #{conditions} and #{temp} degrees"
